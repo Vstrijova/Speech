3df9bd4e1161f6ecf196b61df2bf772d9d223808
Vstrijova <101127668+Vstrijova@users.noreply.github.com>
2022-04-26 12:34:59 +0200

    Update Translate_speech.py

--- Translate_speech.py
+++ Translate_speech.py
@@ -12,4 +12,4 @@ your_text_tospeech = your_text_transl.text
 tts1=gTTS(text = your_text_tospeech, lang = your_lang)
 tts1.save('your_file.mp3')
 audio_file = open('your_file.mp3', 'rb')
-st.audio(data = audio_file, format = "audio/mp3", start_time = 0))
+st.audio(data=audio_file, format="audio/mp3", start_time = 0))
