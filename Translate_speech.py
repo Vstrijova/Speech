054f8b586bc5c7a14cfbf2b5b8b19c3958673046
Vstrijova <101127668+Vstrijova@users.noreply.github.com>
2022-04-26 12:45:04 +0200

    Update Translate_speech.py

--- Translate_speech.py
+++ Translate_speech.py
@@ -5,11 +5,13 @@ from googletrans import Translator
 your_text = st.text_input('Give me a text to translate ')
 your_lang = st.text_input('Give me the 2-letter language code you want to translate into ')
 
-translator = Translator()
-your_text_transl = translator.translate(your_text, dest= your_lang)
-your_text_tospeech = your_text_transl.text
+if you_text != ' ':
+  translator = Translator()
+  your_text_transl = translator.translate(your_text, dest= your_lang)
+  your_text_tospeech = your_text_transl.text
 
-tts1=gTTS(text = your_text_tospeech, lang = your_lang)
-tts1.save('your_file.mp3')
-audio_file = open('your_file.mp3', 'rb')
-st.audio(data=audio_file, format="audio/mp3", start_time = 0)
+  tts1=gTTS(text = your_text_tospeech, lang = your_lang)
+  tts1.save('your_file.mp3')
+  audio_file = open('your_file.mp3', 'rb')
+  st.audio(data=audio_file, format="audio/mp3", start_time = 0)
+  st.download_button(label='Download audio file', data=audio_file, file_name= 'your_file.mp3', mine='audio/mp3')
