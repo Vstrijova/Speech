a03a2b450cd3a38d54163ad98597d53e43791a6d
Vstrijova <101127668+Vstrijova@users.noreply.github.com>
2022-05-03 11:03:59 +0200

    Update Speech_upload.py

--- Speech_upload.py
+++ Speech_upload.py
@@ -12,13 +12,13 @@ if your_uploaded_file is not None:
      st.write(stringio)
 
      if your_uploaded_file != ' ':
-     translator = Translator()
-     your_text_transl = translator.translate(your_uploaded_file, dest= your_lang)
-     your_text_tospeech = your_text_transl.text
-     st.write('Your translated text is: ', your_text_tospeech)
-
-     tts1=gTTS(text = your_text_tospeech, lang = your_lang)
-     tts1.save('your_file.mp3')
+          translator = Translator()
+          your_text_transl = translator.translate(your_uploaded_file, dest= your_lang)
+          your_text_tospeech = your_text_transl.text
+          st.write('Your translated text is: ', your_text_tospeech)
+          
+          tts1=gTTS(text = your_text_tospeech, lang = your_lang
+                    tts1.save('your_file.mp3')
      audio_file = open('your_file.mp3', 'rb')
      st.audio(data=audio_file, format="audio/mp3", start_time = 0)
      st.download_button(label="Download audio file", data=audio_file, file_name='yourfile.mp3',mime='audio/mp3')
